6e7e0d5fa8105d22cc10f3334fadd74a4b6e9b24
kyndv <kyndv@hasaki.vn>
2024-02-27 10:39:08 +0700

    check hidden file or folder

--- file/file.go
+++ file/file.go
@@ -1,14 +1,13 @@
 package utils
 
 import (
-	"archive/zip"
-	"fmt"
-	"io"
 	"os"
-	"path"
 	"path/filepath"
+	"runtime"
+	"syscall"
 )
 
+// CreateFilePath creates a file path if not exist.
 func CreateFilePath(filePath string) error {
 	path, _ := filepath.Split(filePath)
 	if len(path) == 0 {
@@ -43,20 +42,22 @@ func WriteFile(data []byte, filePath string) error {
 	return nil
 }
 
-// Clear file removes the named files.
-func ClearFile(files ...string) error {
-	for _, file := range files {
-		if err := os.Remove(file); err != nil {
+// ClearFiles removes multiple files.
+func ClearFiles(filepaths ...string) error {
+	for _, filepath := range filepaths {
+		if err := os.Remove(filepath); err != nil {
 			return err
 		}
 	}
 	return nil
 }
 
-// Clear folder removes current directory.
-func ClearCurrentFolder(dirPath string) error {
-	if err := os.RemoveAll(dirPath); err != nil {
-		return err
+// ClearFolders removes multiple directories.
+func ClearFolders(dirpaths ...string) error {
+	for _, dirpath := range dirpaths {
+		if err := os.RemoveAll(dirpath); err != nil {
+			return err
+		}
 	}
 	return nil
 }
@@ -125,144 +126,177 @@ func AppendFile(data []byte, filePath string, mode int) error {
 	return nil
 }
 
+const dot_character = 46
 
-// ZipSingleFile zip the sourceFile path to outputFile path
-func ZipSingleFile(sourceFile, outputFile string) error {
-	if err := CreateFilePath(outputFile); err != nil {
-		return err
-	}
-
-	archive, err := os.Create(outputFile)
-	if err != nil {
-		return err
-	}
-	defer archive.Close()
-
-	zipWriter := zip.NewWriter(archive)
-	defer zipWriter.Close()
-
-	f, err := os.Open(sourceFile)
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	w, err := zipWriter.Create(path.Base(f.Name()))
-	if err != nil {
-		return err
-	}
-	if _, err := io.Copy(w, f); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// ZipMultipleFile zip list of sourceFile path to outputFile path
-func ZipMultipleFile(outputFile string, sourceFiles ...string) error {
-	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
-	file, err := os.OpenFile(outputFile, flags, 0644)
-	if err != nil {
-		return fmt.Errorf("failed to open zip for writing: %s", err)
-	}
-	defer file.Close()
-
-	zipw := zip.NewWriter(file)
-	defer zipw.Close()
-
-	for _, filename := range sourceFiles {
-		if err := appendFiles(filename, zipw); err != nil {
-			return fmt.Errorf("failed to add file %s to zip: %s", filename, err)
+// IsHidden returns true if the file or directory is hidden
+func IsHidden(path string) (bool, error) {
+	switch runtime.GOOS {
+	case "windows":
+		// dotfiles also count as hidden
+		if path[0] == dot_character {
+			return true, nil
 		}
-	}
-	return err
-}
 
-func appendFiles(filename string, zipw *zip.Writer) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("failed to open %s: %s", filename, err)
-	}
-	defer file.Close()
-
-	_, filename = filepath.Split(filename)
-	wr, err := zipw.Create(filename)
-	if err != nil {
-		msg := "failed to create entry for %s in zip file: %s"
-		return fmt.Errorf(msg, filename, err)
-	}
-
-	if _, err := io.Copy(wr, file); err != nil {
-		return fmt.Errorf("failed to write %s to zip: %s", filename, err)
-	}
-
-	return nil
-}
-
-func ZZip(folder string, files ...string) error {
-	targetZip := folder //"output.zip"
-
-	// Mở file zip để ghi
-	zipFile, err := os.Create(targetZip)
-	if err != nil {
-		return err
-	}
-	defer zipFile.Close()
-
-	// Tạo biến zip.Writer để ghi vào file zip
-	zipWriter := zip.NewWriter(zipFile)
-	defer zipWriter.Close()
-	for _, file := range files {
-		err = write(zipWriter, file)
+		absPath, err := filepath.Abs(path)
 		if err != nil {
-			return err
+			return false, err
 		}
+
+		// Appending `\\?\` to the absolute path helps with
+		// preventing 'Path Not Specified Error' when accessing
+		// long paths and filenames
+		// https://docs.microsoft.com/en-us/windows/win32/fileio/maximum-file-path-limitation?tabs=cmd
+		pointer, err := syscall.UTF16PtrFromString("\\?\\" + absPath)
+		if err != nil {
+			return false, err
+		}
+		attributes, err := syscall.GetFileAttributes(pointer)
+		if err != nil {
+			return false, err
+		}
+		return attributes&syscall.FILE_ATTRIBUTE_HIDDEN != 0, nil
+	default:
+		return path[0] == dot_character, nil
 	}
-	return nil
 }
 
-func write(zipWriter *zip.Writer, file string) error {
-	fileInfo, err := os.Stat(file)
-	if err != nil {
-		return err
-	}
+// // ZipSingleFile zip the sourceFile path to outputFile path
+// func ZipSingleFile(sourceFile, outputFile string) error {
+// 	if err := CreateFilePath(outputFile); err != nil {
+// 		return err
+// 	}
 
-	// Tạo một header cho file trong file zip
-	header, err := zip.FileInfoHeader(fileInfo)
-	if err != nil {
-		return err
-	}
+// 	archive, err := os.Create(outputFile)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer archive.Close()
 
-	// Đặt tên file trong file zip
-	header.Name = fileInfo.Name()
+// 	zipWriter := zip.NewWriter(archive)
+// 	defer zipWriter.Close()
 
-	// Tạo một đối tượng io.Writer để ghi dữ liệu vào file zip
-	writer, err := zipWriter.CreateHeader(header)
-	if err != nil {
-		return err
-	}
+// 	f, err := os.Open(sourceFile)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer f.Close()
 
-	// Mở file nguồn để đọc dữ liệu
-	sourceFile, err := os.Open(file)
-	if err != nil {
-		return err
-	}
-	defer sourceFile.Close()
+// 	w, err := zipWriter.Create(path.Base(f.Name()))
+// 	if err != nil {
+// 		return err
+// 	}
+// 	if _, err := io.Copy(w, f); err != nil {
+// 		return err
+// 	}
 
-	// Sử dụng buffer để đọc dữ liệu từ file nguồn và ghi vào file zip
-	buffer := make([]byte, 1024)
-	for {
-		bytesRead, err := sourceFile.Read(buffer)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			return err
-		}
-		_, err = writer.Write(buffer[:bytesRead])
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+// 	return nil
+// }
+
+// // ZipMultipleFile zip list of sourceFile path to outputFile path
+// func ZipMultipleFile(outputFile string, sourceFiles ...string) error {
+// 	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+// 	file, err := os.OpenFile(outputFile, flags, 0644)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to open zip for writing: %s", err)
+// 	}
+// 	defer file.Close()
+
+// 	zipw := zip.NewWriter(file)
+// 	defer zipw.Close()
+
+// 	for _, filename := range sourceFiles {
+// 		if err := appendFiles(filename, zipw); err != nil {
+// 			return fmt.Errorf("failed to add file %s to zip: %s", filename, err)
+// 		}
+// 	}
+// 	return err
+// }
+
+// func appendFiles(filename string, zipw *zip.Writer) error {
+// 	file, err := os.Open(filename)
+// 	if err != nil {
+// 		return fmt.Errorf("failed to open %s: %s", filename, err)
+// 	}
+// 	defer file.Close()
+
+// 	_, filename = filepath.Split(filename)
+// 	wr, err := zipw.Create(filename)
+// 	if err != nil {
+// 		msg := "failed to create entry for %s in zip file: %s"
+// 		return fmt.Errorf(msg, filename, err)
+// 	}
+
+// 	if _, err := io.Copy(wr, file); err != nil {
+// 		return fmt.Errorf("failed to write %s to zip: %s", filename, err)
+// 	}
+
+// 	return nil
+// }
+
+// func ZZip(folder string, files ...string) error {
+// 	targetZip := folder //"output.zip"
+
+// 	// Mở file zip để ghi
+// 	zipFile, err := os.Create(targetZip)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer zipFile.Close()
+
+// 	// Tạo biến zip.Writer để ghi vào file zip
+// 	zipWriter := zip.NewWriter(zipFile)
+// 	defer zipWriter.Close()
+// 	for _, file := range files {
+// 		err = write(zipWriter, file)
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
+
+// func write(zipWriter *zip.Writer, file string) error {
+// 	fileInfo, err := os.Stat(file)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Tạo một header cho file trong file zip
+// 	header, err := zip.FileInfoHeader(fileInfo)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Đặt tên file trong file zip
+// 	header.Name = fileInfo.Name()
+
+// 	// Tạo một đối tượng io.Writer để ghi dữ liệu vào file zip
+// 	writer, err := zipWriter.CreateHeader(header)
+// 	if err != nil {
+// 		return err
+// 	}
+
+// 	// Mở file nguồn để đọc dữ liệu
+// 	sourceFile, err := os.Open(file)
+// 	if err != nil {
+// 		return err
+// 	}
+// 	defer sourceFile.Close()
+
+// 	// Sử dụng buffer để đọc dữ liệu từ file nguồn và ghi vào file zip
+// 	buffer := make([]byte, 1024)
+// 	for {
+// 		bytesRead, err := sourceFile.Read(buffer)
+// 		if err == io.EOF {
+// 			break
+// 		}
+// 		if err != nil {
+// 			return err
+// 		}
+// 		_, err = writer.Write(buffer[:bytesRead])
+// 		if err != nil {
+// 			return err
+// 		}
+// 	}
+// 	return nil
+// }
